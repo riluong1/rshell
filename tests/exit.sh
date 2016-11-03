d100a88c81558b6a5a4c7986f5dae264344dfd10
riluong1 <rluong69@gmail.com>
2016-11-02 22:49:13 -0700

    Create exit.sh

---
+++ tests/exit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+exit
+
+echo "Testing exit"; exit
+
+ls && exit
+
+ls -a ; exit
+
+echo "Hi" || exit
+
+soeirekrlke || exit
+
+echo "Hello" && exit ; ls
+
+             exit
+             
+exit                 #ls
+
+ls; pwd || exit
+
+ls ; pwd && exit
+
+aeiorjeao && exit
