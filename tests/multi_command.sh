45d19f53bf55da4c3743e5a4541f36af5ec34f59
riluong1 <rluong69@gmail.com>
2016-11-02 23:08:56 -0700

    Create multi_command.sh

---
+++ tests/multi_command.sh
@@ -0,0 +1,25 @@
+ls -a; echo hello; mkdir test
+
+ls -a; echo hello && mkdir test || echo world; git status
+
+ls ; echo hi; mkdir test
+
+asdasd && pwd
+
+pwd || echo "hello" || ls
+
+ls && fail && echo "This shouldnt work"
+
+ls; null command || echo work
+
+ls; echo "or fail" || echo work
+
+aaeiorj || ajkwe && echo hi
+
+aaeiorj || ajkwe ; echo hi
+
+dont work && dont work ; ls
+
+dont work && dont work || ls
+
+ls -a; echo hello && mkdir test || echo world; pwd || ls && echo stop
