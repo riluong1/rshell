212681f8e5269b72efb1c56d38b0941787489494
riluong1 <rluong69@gmail.com>
2016-11-02 22:53:55 -0700

    Create single_command.sh

---
+++ tests/single_command.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "tests"
+
+ls -a
+
+ls
+
+pwd
+
+ls                                            -l
+
+a;weral;kerlek
+
+ls -l -a
+
+ls -l -a -R
+
+ls -laR
+
+pwd #echo "six"
+
+ls #          ls
+
+mkdir test
+
+rm test
+
+touch newfile
+
+cat newfile
+
+vim Tests.txt
+
+yes
