4141912ae6c203da33f0411d68776914aeb10eae
kevinle90 <kevinle90@gmail.com>
2016-11-18 14:50:09 -0800

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -12,3 +12,20 @@ ls #blah blah
 
 ls -a #beep beep
 
+test -f #meep meep
+
+test #-d
+
+#test -e
+
+(echo A && echo B) || (echo C && echo D)#lalalala
+
+(echo A && echo B) ; (echo C && #echo D)
+
+(echo A && echo B) #|| (echo C && echo D)
+
+(echo A #&& echo B) ; (echo C && echo D)
+
+(echo A) #&& echo B) ; (echo C && echo D)
+
+(echo A)               #&& echo B) ; (echo C && echo D)
